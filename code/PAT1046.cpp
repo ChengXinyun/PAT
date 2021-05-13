9c8ae828bc31139644970b1374f8ffc4daf0eca5
ChengXinyun <3305499121@qq.com>
2021-05-13 20:04:41 +0800

    add notes

--- code/PAT1046.cpp
+++ code/PAT1046.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+//超时的话，想一些简便的计算方法；
+//vector的使用，默认初始化里面为0； 
+//swap()函数
+//min()函数 
 
 int main(){
 	int n;
 	cin >> n;
 	
-	int dis[n+1];
-	int tot = 0;
-	int temp;
-	dis[0] = 0;
-	
+	vector<int> dis(n+1);
+	int tot = 0, cnt, a, b;
 	for(int i = 1; i <= n; i++){
+		int temp;
 		cin >> temp;
 		tot += temp;
 		dis[i] = tot;
 	}
-
-	int cnt;
-	int a, b;
+	
 	cin >> cnt;
 	while(cnt--){
 		cin >> a >> b;
 		if(a > b){
-			int t = a;
-			a = b;
-			b = t;
+			swap(a,b);
 		}
 		int d = dis[b-1] - dis[a-1];
-		d = tot - d > d ? d : tot - d;
+		d = min(d, tot-d);
 		cout << d << endl;
 	}
 	return 0;
