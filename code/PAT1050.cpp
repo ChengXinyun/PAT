dff952d2427c57d3ba0607381eb3ca81c85eceb6
luckyc <luckyc0701@163.com>
2021-07-09 16:48:08 +0800

    add A1050

---
+++ code/PAT1050.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+	char s1[100000], s2[100000];
+	cin.getline(s1, 100000);
+	cin.getline(s2, 100000);
+	// cout << s1 << s2;
+	
+	bool flag[256] ={false};
+	
+    int l1 = strlen(s1);
+    int l2 = strlen(s2);
+	for(int i = 0; i < l2; i++){
+		flag[s2[i]] = true;
+	} 
+	
+	for(int i = 0; i < l1; i++){
+		if(!flag[s1[i]])
+			cout << s1[i];
+	}
+	return 0;
+}
